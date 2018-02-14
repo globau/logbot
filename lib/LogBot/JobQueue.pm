a25c5dd146a577ded857a392f1ce112fdb9f30cb
Byron Jones <byron@glob.com.au>
2018-02-14 19:25:13 +0800

    refactor jobqueue into class

--- lib/LogBot/JobQueue.pm
+++ lib/LogBot/JobQueue.pm
@@ -12,23 +12,23 @@ use JSON::XS qw( decode_json encode_json );
 use LogBot::Util qw( path_for slurp spurt timestamp );
 use Time::HiRes qw( time );
 
-our @EXPORT_OK = qw(
-    publish_job fetch_job delete_job
-);
-use parent 'Exporter';
+sub new {
+    my ($class, $config) = @_;
+    return bless({ config => $config }, $class);
+}
 
 sub publish_job {
-    my ($config, $data) = @_;
+    my ($self, $data) = @_;
     my $file;
     do {
-        $file = path_for($config, 'queue') . '/' . $$ . '.' . time() . '.json';
+        $file = path_for($self->{config}, 'queue') . '/' . $$ . '.' . time() . '.json';
     } while (-e $file);
     spurt($file, encode_json($data));
 }
 
 sub fetch_job {
-    my ($config) = @_;
-    my $spec = path_for($config, 'queue') . '/*.json';
+    my ($self) = @_;
+    my $spec = path_for($self->{config}, 'queue') . '/*.json';
     my $quit = 0;
     local $SIG{INT} = sub { $quit = 1 };
     while (!$quit) {
@@ -42,8 +42,8 @@ sub fetch_job {
 }
 
 sub delete_job {
-    my ($config, $job) = @_;
-    unlink(path_for($config, 'queue') . '/' . $job)
+    my ($self, $job) = @_;
+    unlink(path_for($self->{config}, 'queue') . '/' . $job)
         || say timestamp(), ' !! ', "failed to delete $job: $!";
 }
 
